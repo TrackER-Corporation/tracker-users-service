dc414eb9daf3a6064162c5a49da7f2e3daef78bb
Dallas <71103219+LeleDallas@users.noreply.github.com>
2023-04-24 16:54:33 +0200

    fix: add catch handler

--- db/services/database.service.ts
+++ db/services/database.service.ts
@@ -6,15 +6,19 @@ export const collections: { cards?: mongoDB.Collection } = {}
 export async function connectToDatabase() {
   dotenv.config();
 
-  const client: mongoDB.MongoClient = new mongoDB.MongoClient(process.env.DB_CONN_STRING!);
+  try {
+    const client: mongoDB.MongoClient = new mongoDB.MongoClient(process.env.DB_CONN_STRING!);
 
-  await client.connect();
+    await client.connect();
 
-  const db: mongoDB.Db = client.db(process.env.DB_NAME);
+    const db: mongoDB.Db = client.db(process.env.DB_NAME);
 
-  const cardsCollection: mongoDB.Collection = db.collection(process.env.CARDS_COLLECTION_NAME!);
+    const cardsCollection: mongoDB.Collection = db.collection(process.env.CARDS_COLLECTION_NAME!);
 
-  collections.cards = cardsCollection;
-  
-  console.log(`Successfully connected to database: ${db.databaseName} and collection: ${cardsCollection.collectionName}`);
+    collections.cards = cardsCollection;
+
+    console.log(`Successfully connected to database: ${db.databaseName} and collection: ${cardsCollection.collectionName}`);
+  } catch (error) {
+    console.error(error);
+  }
 }

0fcb091760398f46a658b97940c119911f02a525
Dallas <71103219+LeleDallas@users.noreply.github.com>
2023-04-24 16:28:31 +0200

    refactor: update config

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,13 @@ export default defineConfig({
     coverage: {
       provider: "c8",
       all: true,
+      lines: 75,
+      branches: 75,
+      functions: 75,
+      statements: 75,
       include: ["**/*.ts"],
       exclude: ["**/types/*.ts", "**/test/*.ts", "vite-env.d.ts", "vite.config.ts", "**/db/**/*.ts"],
+      reporter: ['text', 'json-summary', 'json'],
     },
   },
   server: {

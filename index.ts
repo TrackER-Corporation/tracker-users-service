6965d2d0ac100c4d756fcafc88aef4a3edbd725d
Dallas <71103219+LeleDallas@users.noreply.github.com>
2023-04-24 15:48:51 +0200

    feat: micro-service setup

---
+++ index.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import { connectToDatabase } from './db/services/database.service';
+
+dotenv.config();
+
+const port = process.env.PORT;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use(
+  '/',
+  (req, res, next) => {
+    return res.status(200).json({ "msg": "test" })
+  });
+
+connectToDatabase()
+app.listen(port, () => console.info(`tracker-users-service is running`));

07878d3d260418909403032c4804eaace8a93c3b
Dallas <71103219+LeleDallas@users.noreply.github.com>
2023-04-29 18:23:38 +0200

    test: middleware

--- middleware/authMiddleware.ts
+++ middleware/authMiddleware.ts
@@ -12,15 +12,12 @@ export const protect = asyncHandler(async (req: any, res: any, next: any) => {
       // Get token from header
       token = req.headers.authorization.split(' ')[1]
       // Verify token
-      console.log(token)
       const decoded = jwt.verify(token, process.env.JWT_SECRET as string)
       // Get user from the token
-      console.log(decoded)
       req.user = await collections?.users?.findById(decoded.id).select('-password')
       console.log(req.user)
       next()
     } catch (error) {
-      console.log(error)
       res.status(401)
       throw new Error('Not authorized')
     }
---
+++ test/authMiddleware.test.ts
@@ -0,0 +1,56 @@
+import jwt from 'jsonwebtoken';
+import { collections } from '../db/services/database.service';
+import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+import { protect } from '../middleware/authMiddleware';
+import dotenv from "dotenv"
+
+dotenv.config()
+
+describe('protect middleware', () => {
+    let req: any, res: any, next: any, token: any;
+    let collections: any;
+
+    beforeAll(() => {
+        collections = {
+            users: {
+                findById: vi.fn(),
+            },
+        };
+        req = {
+            headers: {},
+            user: null,
+        };
+        res = {
+            status: vi.fn().mockReturnThis(),
+            send: vi.fn(),
+        };
+        next = vi.fn();
+        token = jwt.sign({ id: '123' }, process.env.JWT_SECRET as string);
+    });
+
+    test('should call next if token is valid', async () => {
+        req.headers.authorization = `Bearer ${token}`;
+        const user = { id: '123', name: 'John Doe', email: 'john@example.com' };
+        vi.spyOn(collections.users, 'findById').mockResolvedValue(user);
+        const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
+
+        await protect(req, res, next);
+
+        // expect(req.user).toEqual(user);
+        // expect(collections.users?.findById).toHaveBeenCalledWith(decoded.id);
+        expect(next).toHaveBeenCalled();
+    });
+
+    test('should return 401 if token is invalid', async () => {
+        req.headers.authorization = `Bearer invalidToken`;
+        await protect(req, res, next);
+        expect(res.status).toHaveBeenCalledWith(401);
+        expect(next).toHaveBeenCalledWith(new Error('Not authorized'));
+    });
+
+    test('should return 401 if no token is provided', async () => {
+        await protect(req, res, next);
+        expect(res.status).toHaveBeenCalledWith(401);
+        expect(next).toHaveBeenCalledWith(new Error('Not authorized'));
+    });
+});
